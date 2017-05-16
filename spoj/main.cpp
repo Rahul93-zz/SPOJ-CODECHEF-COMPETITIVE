774105bfa01bb1a2b81f1d9bc43f7d4a3b410626
Rahul Kathuria <rahulkathuria@Rahuls-MacBook-Air.local>
2017-05-17 02:05:14 +0530

    Initial Commit

---
+++ spoj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  spoj
+//
+//  Created by Rahul  Kathuria on 17/05/17.
+//  Copyright © 2017 Rahul  Kathuria. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
